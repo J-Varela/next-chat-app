19fd13f01757556837ecbb1cde6e0de11246fde0
Jonathan Varela <jonvarela4@gmail.com>
2024-02-18 22:41:27 -0500

    import Imessage

--- components/ListMessages.tsx
+++ components/ListMessages.tsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from 'react';
-import { useMessage } from '@/lib/store/messages';
+import React, { useRef, useState, useEffect } from 'react';
+import { Imessage, useMessage } from '@/lib/store/messages';
 import { supabaseBrowser } from '@/lib/supabase/browser';
+import { toast } from 'sonner';
+import { ArrowDown } from 'lucide-react';
 
 export default function ListMessages() {
   const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
@@ -16,6 +18,58 @@ export default function ListMessages() {
   } = useMessage((state) => state);
 
   const supabase = supabaseBrowser();
+  useEffect(() => {
+    const channel = supabase
+      .channel('chat-room')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'messages' },
+        async (payload) => {
+          if (!optimisticIds.includes(payload.new.id)) {
+            const { error, data } = await supabase
+              .from('users')
+              .select('*')
+              .eq('id', payload.new.send_by)
+              .single();
+            if (error) {
+              toast.error(error.message);
+            } else {
+              const newMessage = {
+                ...payload.new,
+                users: data,
+              };
+              addMessage(newMessage as Imessage);
+            }
+          }
+          const scrollContainer = scrollRef.current;
+          if (
+            scrollContainer.scrollTop <
+            scrollContainer.scrollHeight - scrollContainer.clientHeight - 10
+          ) {
+            setNotification((current) => current + 1);
+          }
+        }
+      )
+      .on(
+        'postgres_changes',
+        { event: 'DELETE', schema: 'public', table: 'messages' },
+        (payload) => {
+          optimisticDeleteMessage(payload.old.id);
+        }
+      )
+      .on(
+        'postgres_changes',
+        { event: 'UPDATE', schema: 'public', table: 'messages' },
+        (payload) => {
+          optimisticUpdateMessage(payload.new as Imessage);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [messages]);
 
   return <div></div>;
 }
