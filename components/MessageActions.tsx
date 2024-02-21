de9d3aafa56fc25fd43102581f8a0221435b1cc8
Jonathan Varela <jonvarela4@gmail.com>
2024-02-21 14:25:02 -0500

    Implement handleEdit function for editing messages

--- components/MessageActions.tsx
+++ components/MessageActions.tsx
@@ -73,3 +73,56 @@ export function DeleteAlert() {
     </AlertDialog>
   );
 }
+
+export function EditAlert() {
+  const actionMessage = useMessage((state) => state.actionMessage);
+  const optimisticUpdateMessage = useMessage(
+    (state) => state.optimisticUpdateMessage
+  );
+
+  const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  const handleEdit = async () => {
+    const supabase = supabaseBrowser();
+    const text = inputRef.current.value.trim();
+    if (text) {
+      optimisticUpdateMessage({
+        ...actionMessage,
+        text,
+        is_edit: true,
+      } as Imessage);
+      const { error } = await supabase
+        .from('messages')
+        .update({ text, is_edit: true })
+        .eq('id', actionMessage?.id!);
+      if (error) {
+        toast.error(error.message);
+      } else {
+        toast.success('Update Successfully');
+      }
+      document.getElementById('trigger-edit')?.click();
+    } else {
+      document.getElementById('trigger-edit')?.click();
+      document.getElementById('trigger-delete')?.click();
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button id="trigger-edit"></button>
+      </DialogTrigger>
+      <DialogContent className="w-full">
+        <DialogHeader>
+          <DialogTitle>Edit Message</DialogTitle>
+        </DialogHeader>
+        <Input defaultValue={actionMessage?.text} ref={inputRef} />
+        <DialogFooter>
+          <Button type="submit" onClick={handleEdit}>
+            Save Changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
